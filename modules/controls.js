b498259c351b1226d5dbe28ebb55e04a83334a62
Eneroth3 <eneroth3@gmail.com>
2018-02-17 12:54:20 +0100

    Support text inputs

--- modules/controls.js
+++ modules/controls.js
@@ -1,14 +1,44 @@
 function Controls( options ) {
+  // Should underline be shown since init or only when holding down alt?
 
+  /*
+   * Test if element is a controller supported by this lib.
+   */
   function isControl(c) {
-    // For now only support buttons.
-    // Later5 support input, textarea and maybe link.
-    return c.tagName == 'BUTTON';
+    // Implement textarea and maybe link too.
+    // Test on textarea, number input, date input, slider input, checkbox input, radio input.
+    return $.inArray( c.tagName, [ 'BUTTON', 'INPUT' ] ) !== -1;
   }
 
+  /*
+   * Get the node being the label of a control
+   * (the node which innerHTML the access key should be underlined in).
+   */
   function labelNode(c) {
-    // On inputs, return the associated label. On button, return button itself.
-    return c;
+    c = $(c)
+    var label = $("label[for='"+c.attr('id')+"']");
+    if (label.length == 0) label = c.closest('label');
+
+    return (label.length == 0) ? c.context : label[0];
+  }
+
+  /*
+   * Activate a control.
+   * Simulate click on buttons and links, focus text inputs.
+   */
+  function activateControl(c) {
+    // Test if this can trigger both events defined by onclick attribute,
+    // "normal" events (whatever those are called), and event set by this
+    // lib (sketchup.{classname - 'button'}).
+    // Should set focus to text inputs.
+    switch(c.tagName) {
+      case 'BUTTON':
+      // A ?
+        c.click();
+      case 'INPUT':
+      case 'TEXTAREA':
+        c.focus();
+    }
   }
 
   function initAccessKey(c, accessKey) {
@@ -16,14 +46,12 @@ function Controls( options ) {
     var regExp = RegExp(accessKey, 'i');
     ln.innerHTML = ln.innerHTML.replace(regExp , '<u>$&</u>');
 
+    // FIXME: access key doesn't work for inputs inside of labels.
+    // This is probably an effect of changing innerHTML.
     $(document).keydown( { accessKey: accessKey, c: c } , function(e) {
       if (!e.altKey) return;
       if (e.key != e.data.accessKey) return;
-      // Test if this can trigger both events defined by onclick attribute,
-      // "normal" events (whatever those are called), and event set by this
-      // lib (sketchup.{classname - 'button'}).
-      // Should set focus to text inputs.
-      e.data.c.click();
+      activateControl(e.data.c);
       e.preventDefault();
     });
   }
@@ -32,7 +60,9 @@ function Controls( options ) {
     // If click event isn't already defined, and button has a class
     // prefixed with 'button-' (e.g. 'button-ok'),
     // call Sketchup.<className excluding button> (e.g. sketchup.ok').
-    c.onclick = function() {alert(this.innerHTML);};
+    if (c.tagName == 'BUTTON'){
+      c.onclick = function() {alert(this.innerHTML);};
+    }
 
     if (options.accessKeys) {
       var accessKey = c.getAttribute('data-access-key');
